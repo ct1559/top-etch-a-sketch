0ceb9ef5203db600a5b78988ffe981878b7cf581
ct1559 <e.cooper.taylor@gmail.com>
2022-06-05 13:48:54 -0400

    Add hover effect to divs inside container
    Divs inside the grid change to the color black
    when you mouse over them

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
-document.body.onload = createGrid();
+const gridContainer = document.querySelector(".grid-container");
 
 // Create 256 div elements
 function createGrid() {
-  const gridContainer = document.querySelector(".grid-container");
   const gridItem = document.createElement("div");
   gridItem.classList.add("grid-item");
   gridContainer.appendChild(gridItem);
-
-  console.log(gridContainer);
 
   // Cloning gridItem 255 times
   for (let i = 0; i < 255; i++) {
     gridContainer.appendChild(gridItem.cloneNode(true));
   }
 }
+
+// Create the 256 divs when page loads
+document.body.onload = createGrid();
+
+// Change background color when mouse hovers over a div
+const gridItems = document.querySelectorAll(".grid-item");
+gridItems.forEach(function (currentValue, currentIndex, listObj) {
+  currentValue.addEventListener("mouseenter", function (e) {
+    console.log("change background color");
+    e.target.style.backgroundColor = "black";
+  });
+});
